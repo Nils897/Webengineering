9ac74fefc3f8444fb62e33492142aa9fa0976db1
Nils897 <161706814+Nils897@users.noreply.github.com>
2025-03-04 09:17:49 +0100

    Update index.js

--- View/Skript/index.js
+++ View/Skript/index.js
@@ -58,14 +58,4 @@ function loadPage(page) {
         .catch(error => console.error("Fehler beim Laden der Seite:", error));
 }
 
-function loadPage(page) {
-    fetch(page)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("content").innerHTML = data;
-        })
-        .catch(error => console.error("Fehler beim Laden der Seite:", error));
-}
-
-
 document.addEventListener("DOMContentLoaded", loadLayout);
